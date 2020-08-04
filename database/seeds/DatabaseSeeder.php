d029f60f7c84a1cbfddb612bbe347fd1f30d70e2
zayaserg <zayaserg@gmail.com>
2020-08-04 13:08:45 +0300

    Generate fake answers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,15 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         factory(App\User::class, 3)->create()->each(function($u) {
-            $u->questions()->saveMany(
+            $u->questions()
+            ->saveMany(
                 factory(App\Question::class, rand(1, 5))->make()
-            );
+            )
+            ->each(function ($q) {
+                $q->answers()->saveMany(
+                    factory(App\Answer::class, rand(1,5))->make()
+                );
+            });
         });
         // factory(App\Question::class, 10)->create();
     }
