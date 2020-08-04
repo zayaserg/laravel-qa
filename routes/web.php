9f8a346d3c4ecb4e468d9ae32009618d76d38fba
zayaserg <zayaserg@gmail.com>
2020-08-04 20:14:41 +0300

    Display answers for question

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
+
+// Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+// Route::resource('questions.answers', 'AnswersController')->only(['store', 'edit', 'update', 'destroy']);
+Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
+
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
